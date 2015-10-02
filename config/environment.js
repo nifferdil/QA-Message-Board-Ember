e314121a96b80110256df6619b6022fe8311daa5
nifferdil <nifferdil@gmail.com>
2015-10-02 15:25:57 -0700

    add initial files

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'question-answer',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://qa-message-board-1928.firebaseIO.com',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
