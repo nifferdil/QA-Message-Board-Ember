738a50a8a3808f539a82575f5d0d0f811d2ca78c
nifferdil <nifferdil@gmail.com>
2015-10-02 21:20:38 -0700

    each question has a page

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -6,12 +6,14 @@ export default Ember.Component.extend({
     updateQuestionForm() {
       this.set('updateQuestionForm', true);
     },
+
     update(question) {
       var params = {
         question: this.get('question'),
         author: this.get('author'),
-        notes: this.get('notes')
+        notes: this.get('notes'),
       };
+      
       this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
     }
--- app/models/question.js
+++ app/models/question.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   question: DS.attr(),
-  answer: DS.attr(),
+  author: DS.attr(),
   notes: DS.attr()
 });
